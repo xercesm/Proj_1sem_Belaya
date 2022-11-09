8f4df58c853e81347f9b0af4bc5248f86f837d43
xercesm <belaya.aliss@yandex.ru>
2022-11-09 14:22:27 +0300

    PZ_6_1 ended

---
+++ PZ_6/PZ_6_2.py
@@ -0,0 +1,19 @@
+#Дан список размера N. Найти номера тех элементов списка, которые больше своего
+#левого соседа, и количество таких элементов. Найденные номера выводить в
+#порядке их убывания.
+
+import random
+
+n = random.randrange(2,11)
+print('n = ', n)
+
+a = [random.randrange(1,11) for i in range(n)]
+print(a)
+
+c = 0
+for i in range(len(a)-1,0,-1):
+    if a[i] > a[i-1]:
+        c += 1
+        print(i,end='; ')
+print()
+print('Count:',c)
