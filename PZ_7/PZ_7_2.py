af34c7b0fe239f45c9957120f7b6b1eb430e4e77
xercesm <belaya.aliss@yandex.ru>
2022-11-26 14:31:27 +0300

    commit

--- PZ_7/PZ_7_2.py
+++ PZ_7/PZ_7_2.py
@@ -3,59 +3,29 @@
 # закрывающая), то вывести число 0. В противном случае вывести или номер позиции,
 # в которой расположена первая ошибочная закрывающая скобка, или, если
 # закрывающих скобок не хватает, число —1.
-a = list(input())
-b = a.count('(')
-c = a.count(')')
-#print(b, c)
-if c < b:
-    print(-1)
-elif c > b:
-    while b != 0:
-        a.remove(')')
-        b -= 1
-    print(a)
-    print(a.index(')'))
-else:
-    print(0)
 
+st = input('Введите строку: ') #создание строки, ввод данных
+lst_1 = [] #создание 1 пустого списка
+lst_2 = [] #создание 2 пустого списка
+for i in st: #цикл, в котором i проходит через всю строку и перебирает все значения
+    if i == ')': #если i равняется )
+        lst_1.append(i) #
+    elif i == '(': #также если i равняется (
+        lst_2.append(i) #
 
-# s=input("Введите строку: ")
-# v = 0
-# list_1 = []
-# list_2 = []
-# while True:
-#     while v < len(s):
-#         if s[v] == ")":
-#             print(v)
-#             break
-#         elif v == len(s):
-#             break
-#         v += 1
-#         break
-#     for i in s:
-#         if (i=="("):
-#             list_1.append(i)
-#         elif (i == ")"):
-#             list_2.append(i)
-#     for n in s:
-#        if (n==")"):
-#         list_2.append(n)
-#
-#         if len(list_1) == len(list_2):
-#             print(0)
-#             break
-#        else:
-#            print(1)
-#        break
-#     break
-#
-#
-#     # else:
-#     #     if (i==")"):
-#     #         v=v-1
-#     #         for i in s[s.index(")"):]:
-#     #             if (i=="("):
-#     #                 v=v+1
-#
-#
-# # print(v)
+if len(lst_1) == len(lst_2): #если список_1 равен список_2
+    print(0) #выводим 0
+elif len(lst_1) <len(lst_2): #если список_1 меньше, чем список_2
+    print(1) #выводим 1
+else: #Иначе
+    n = 0
+    number = 0
+    while n < len(st): #повторение цикла 
+        if st[n] == '(':
+            number += 1
+        elif st[n] == ')':
+            number += 1
+            if number % 2 != 0:
+                print(n)
+                break
+        n += 1
