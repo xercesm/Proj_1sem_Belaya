54a46b3befd02c02a2a67f05e97756390bd65941
zaitcevavic <zaitcev.igor18@gmail.com>
2023-01-20 17:49:50 +0300

    commit

--- PZ_11/PZ_11_2.py
+++ PZ_11/PZ_11_2.py
@@ -4,21 +4,29 @@
 
 t = 0
 d = 0
+q = 1
+L = []
+D = []
 for i in open('text18-2.txt', encoding='UTF-8'):
     print(i, end='')
     t += 1
     for j in i:
-        if j in ",.!?;:.....-":
+        if j in ",.!?;:—….....":
             d += 1
 print(end='\n')
 print('Количество строк: ', t, end='\n')
 print('Количество знаков препинаний : ', d, end='\n')
 
-f1 = open('text18-2.txt', encoding='UTF-8')
-l = f1.readlines()
-l[0], l[3] = l[3], l[0]
-f1.close()
+f1 = open('text18-2.txt', encoding="UTF-8")
+for x in f1:
+    L.append(x)
+f2 = open('text18-3.txt', 'w', encoding="UTF-8")
+for x in L:
+    D.append(L[-q])
+    q -=1
 
-f2 = open('text18-3.txt', 'w')
-f2.writelines(l)
+for x in D:
+    f2.writelines(x)
+f1.close()
 f2.close()
+
